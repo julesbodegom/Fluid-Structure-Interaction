dfa256015a9652792a34a3e9a41a768c708f2a27
julesbodegom <julesbodegom@hotmail.nl>
2026-03-19 12:08:13 +0100

    until 2.2

--- MatlabExercise3/matlab/DGCL-1D/dgcl_BE.m
+++ MatlabExercise3/matlab/DGCL-1D/dgcl_BE.m
@@ -4,16 +4,14 @@ Amp = 0;        % amplitude of the initial solution variation
 A_m = 0.1;      % amplitude of the mesh motion
 u_a = 0;        % advection velocity
 
-dt   = 0.1;     % time step
 tend = 2;       % total simulation time
 
-fmt = 'b-';     % linecolor for plot of final solution
+show_sol = 0;   % (=0) turned off to speed up loop execution
 
-show_sol = 1;   % (=1) show intermediate solutions during simulation
-
-method = 1;     % method = 1 : exact at t(n+1)
+method = 3;     % method = 1 : exact at t(n+1)
                 % method = 2 : exact at t(n+1/2)
                 % method = 3 : DGCL
+
 if (method == 1)
   disp('Mesh velocities are EXACT at t(n+1)');
 elseif (method == 2)
@@ -22,98 +20,88 @@ else
   disp('Mesh velocities satisfy DGCL');
 end
 
-%% Initialisation
+%% Setup for the 3 requested runs
+dt_array  = [0.1, 0.05, 0.025];   % The three time steps
+fmt_array = {'b-', 'r-', 'g-'};   % The three plot line styles
 
-dx   = zeros(1,N);    % cell volumes
-xi   = zeros(1,N+1);  % face centers
-x    = zeros(1,N);    % cell centers
-
-xi  = (0:N)/N;              % face centers
-xi0 = (0:N)/N;              % face centers at t=0
-x   = (1:N)/N - 0.5/N;      % cell centers
-dx  = xi(2:N+1) - xi(1:N);  % cell volumes
-
-% determine the initial condition at the cell center locations x
-rho0 = 1 + Amp*sin(x*2*pi); % initial solution
-rho  = rho0;                % set solution to initial solution
-
-
-% Plot initial solution
-figure(1);
-hold off;
-axis([0 1 0.8-Amp 1.2+Amp]);
-plot(x,rho,'-x');
-hold off;
-
-L = zeros(N);   % Discretization matrix
-
-%% Simulation loop
-for t=dt:dt:tend
-  dx_tn  = dx;  % store cell volume at tn
-  xi_tn  = xi;  % store face center at tn
-  rho_tn = rho; % store solution at tn
-  
-  xi     = xi0 + A_m * sin(2*pi*t) * sin(2*pi*xi0);   % face centers at tn+1
-  dx     = xi(2:N+1)-xi(1:N);                         % cell volumes at tn+1
-  x      = xi(1:N) + dx/2;                            % cell centers at tn+1
-  
-  dxidt_exnp1   = 2*pi*A_m*cos(2*pi*(t))*sin(2*pi*xi0);         % exact face velocity at tn+1
-  dxidt_exnp1_2 = 2*pi*A_m*cos(2*pi*(t-dt/2))*sin(2*pi*xi0);    % exact face velocity at tn+1/2
-  dxidt_dgcl    = (xi - xi_tn) / dt;                            % face velocity satisfying D-GCL
-
-  if (method == 1)
-    dxidt = dxidt_exnp1;
-  elseif (method ==2)
-    dxidt = dxidt_exnp1_2;
-  else
-    dxidt = dxidt_dgcl;
-  end
-
-  % define the relative face velocity that includes mesh velocity and
-  % advection velocity
-  dxidt_r = u_a - dxidt;
-    
-  % Setting up system to solve; for each cell:
-  %
-  %   (rho*dx)^(n+1) - (rho*dx)^n      
-  %   --------------------------- + [ rho_leftface * (dxidt_r)_leftface * (-1) + rho_rightface * (dxidt_r)_rightface * (+1) ]^(n+1) = 0
-  %             dt                 
-  %
-  % which can be written for the total system as:
-  %
-  % L rho^(n+1) = (rho*dx)^n
-  %
-  % We use a simple avarage to compute the solution at a cell face:
-  %   rho_face = 0.5 * (rho_leftcell + rho_rightcell)
-  %
-  % Internal cells
-  for i=2:N-1
-    L(i,i-1:i+1) = [0 dx(i) 0] + dt * 0.5*[-dxidt_r(i) dxidt_r(i+1)-dxidt_r(i) dxidt_r(i+1)];
-  end
-  % Boundary cells, use periodicity, i.e. index 0 => N, and index N+1 => 1
-  L(1,1:2)   = [dx(1) 0] + dt * 0.5*[dxidt_r(2)-dxidt_r(1) dxidt_r(2)];
-  L(1,N)     =           - dt * 0.5*dxidt_r(1);
-  L(N,N-1:N) = [0 dx(N)] + dt * 0.5*[-dxidt_r(N) dxidt_r(1)-dxidt_r(N)];
-  L(N,1)     =           + dt * 0.5*dxidt_r(1);
-
-  % solve system
-  rho = (L\(dx_tn.*rho_tn)')';
-
-  % Show intermediate solution
-  if (show_sol)
-    figure(1);
-    hold off;
-    plot(x,rho,'-x');
-    axis([0 1 0.8-Amp 1.2+Amp]);
-%    pause
-  end
-  
-end
-
-%% Plot final solution
+% Prepare the final plot figure (Figure 2)
 figure(2);
+clf; % Clear the figure so you get a fresh plot every time you run the script
 hold on;
-title(['Solution at t=' num2str(tend)]);
+title(['Solution at t=' num2str(tend) ' for Method ' num2str(method)]);
 ylabel('Solution');
 xlabel('x');
-plot(x,rho,fmt);
+
+%% Outer loop for different time steps
+for run = 1:length(dt_array)
+    dt = dt_array(run);
+    fmt = fmt_array{run};
+    
+    disp(['Running simulation for dt = ', num2str(dt)]);
+
+    %% Initialisation (Reset for each new dt)
+    dx   = zeros(1,N);    % cell volumes
+    xi   = zeros(1,N+1);  % face centers
+    x    = zeros(1,N);    % cell centers
+
+    xi  = (0:N)/N;              % face centers
+    xi0 = (0:N)/N;              % face centers at t=0
+    x   = (1:N)/N - 0.5/N;      % cell centers
+    dx  = xi(2:N+1) - xi(1:N);  % cell volumes
+
+    % determine the initial condition at the cell center locations x
+    rho0 = 1 + Amp*sin(x*2*pi); % initial solution
+    rho  = rho0;                % set solution to initial solution
+
+    L = zeros(N);   % Discretization matrix
+
+    %% Simulation loop
+    for t=dt:dt:tend
+      dx_tn  = dx;  % store cell volume at tn
+      xi_tn  = xi;  % store face center at tn
+      rho_tn = rho; % store solution at tn
+      
+      xi     = xi0 + A_m * sin(2*pi*t) * sin(2*pi*xi0);   % face centers at tn+1
+      dx     = xi(2:N+1)-xi(1:N);                         % cell volumes at tn+1
+      x      = xi(1:N) + dx/2;                            % cell centers at tn+1
+      
+      dxidt_exnp1   = 2*pi*A_m*cos(2*pi*(t))*sin(2*pi*xi0);         % exact face velocity at tn+1
+      dxidt_exnp1_2 = 2*pi*A_m*cos(2*pi*(t-dt/2))*sin(2*pi*xi0);    % exact face velocity at tn+1/2
+      dxidt_dgcl    = (xi - xi_tn) / dt;                            % face velocity satisfying D-GCL
+
+      if (method == 1)
+        dxidt = dxidt_exnp1;
+      elseif (method ==2)
+        dxidt = dxidt_exnp1_2;
+      else
+        dxidt = dxidt_dgcl;
+      end
+
+      % define the relative face velocity that includes mesh velocity and
+      % advection velocity
+      dxidt_r = u_a - dxidt;
+        
+      % Internal cells
+      for i=2:N-1
+        L(i,i-1:i+1) = [0 dx(i) 0] + dt * 0.5*[-dxidt_r(i) dxidt_r(i+1)-dxidt_r(i) dxidt_r(i+1)];
+      end
+      % Boundary cells, use periodicity, i.e. index 0 => N, and index N+1 => 1
+      L(1,1:2)   = [dx(1) 0] + dt * 0.5*[dxidt_r(2)-dxidt_r(1) dxidt_r(2)];
+      L(1,N)     =           - dt * 0.5*dxidt_r(1);
+      L(N,N-1:N) = [0 dx(N)] + dt * 0.5*[-dxidt_r(N) dxidt_r(1)-dxidt_r(N)];
+      L(N,1)     =           + dt * 0.5*dxidt_r(1);
+
+      % solve system
+      rho = (L\(dx_tn.*rho_tn)')';
+    end
+
+    %% Plot final solution for this dt
+    figure(2);
+    plot(x,rho,fmt, 'DisplayName', ['dt = ' num2str(dt)]);
+    
+end
+
+% Finalize the plot format
+figure(2);
+legend('show', 'Location', 'best');
+hold off;
---
+++ MatlabExercise3/matlab/DGCL-1D/dgcl_BE_ex22.m
@@ -0,0 +1,113 @@
+%% Input data
+N   = 20;       % number of cells
+Amp = 0;        % amplitude of the initial solution variation
+A_m = 0.1;      % amplitude of the mesh motion
+u_a = 0;        % advection velocity
+tend = 2;       % total simulation time
+
+% Time step refinement parameters
+k_vals = 0:6;
+dt_array = 0.1 * 2.^(-k_vals);
+colors = {'b', 'r', 'g'}; % b=Method 1, r=Method 2, g=Method 3
+
+% Prepare the log-log plot figure
+figure(3);
+clf;
+hold on;
+title('Time Step Refinement Study: Error vs \Delta t');
+xlabel('\Delta t');
+ylabel('Root-Mean-Square Error');
+set(gca, 'XScale', 'log', 'YScale', 'log'); % Ensure log-log axes
+grid on;
+
+%% Outer loop for the 3 methods
+for method = 1:3
+    
+    % Arrays to store the computed errors for this method
+    err1_array = zeros(1, length(k_vals));
+    err2_array = zeros(1, length(k_vals));
+    
+    %% Inner loop for different time steps
+    for k_idx = 1:length(k_vals)
+        dt = dt_array(k_idx);
+        M = round(tend / dt); % Total number of time steps
+
+        % Initialisation
+        dx   = zeros(1,N);    % cell volumes
+        xi   = zeros(1,N+1);  % face centers
+        x    = zeros(1,N);    % cell centers
+
+        xi  = (0:N)/N;              % face centers
+        xi0 = (0:N)/N;              % face centers at t=0
+        x   = (1:N)/N - 0.5/N;      % cell centers
+        dx  = xi(2:N+1) - xi(1:N);  % cell volumes
+
+        % determine the initial condition at the cell center locations x
+        rho0 = 1 + Amp*sin(x*2*pi); % initial solution
+        rho  = rho0;                % set solution to initial solution
+
+        L = zeros(N);   % Discretization matrix
+        
+        sum_sq_err = 0; % Variable to accumulate error for epsilon_2
+
+        %% Simulation loop
+        for t=dt:dt:tend
+            dx_tn  = dx;  % store cell volume at tn
+            xi_tn  = xi;  % store face center at tn
+            rho_tn = rho; % store solution at tn
+            
+            xi     = xi0 + A_m * sin(2*pi*t) * sin(2*pi*xi0);   % face centers at tn+1
+            dx     = xi(2:N+1)-xi(1:N);                         % cell volumes at tn+1
+            x      = xi(1:N) + dx/2;                            % cell centers at tn+1
+            
+            dxidt_exnp1   = 2*pi*A_m*cos(2*pi*(t))*sin(2*pi*xi0);         % exact face velocity at tn+1
+            dxidt_exnp1_2 = 2*pi*A_m*cos(2*pi*(t-dt/2))*sin(2*pi*xi0);    % exact face velocity at tn+1/2
+            dxidt_dgcl    = (xi - xi_tn) / dt;                            % face velocity satisfying D-GCL
+
+            if (method == 1)
+                dxidt = dxidt_exnp1;
+            elseif (method ==2)
+                dxidt = dxidt_exnp1_2;
+            else
+                dxidt = dxidt_dgcl;
+            end
+
+            % define the relative face velocity
+            dxidt_r = u_a - dxidt;
+              
+            % Internal cells
+            for i=2:N-1
+                L(i,i-1:i+1) = [0 dx(i) 0] + dt * 0.5*[-dxidt_r(i) dxidt_r(i+1)-dxidt_r(i) dxidt_r(i+1)];
+            end
+            % Boundary cells, use periodicity
+            L(1,1:2)   = [dx(1) 0] + dt * 0.5*[dxidt_r(2)-dxidt_r(1) dxidt_r(2)];
+            L(1,N)     =           - dt * 0.5*dxidt_r(1);
+            L(N,N-1:N) = [0 dx(N)] + dt * 0.5*[-dxidt_r(N) dxidt_r(1)-dxidt_r(N)];
+            L(N,1)     =           + dt * 0.5*dxidt_r(1);
+
+            % solve system
+            rho = (L\(dx_tn.*rho_tn)')';
+            
+            % Accumulate error for epsilon_2 (Exact solution is exactly 1)
+            sum_sq_err = sum_sq_err + sum((rho - 1).^2);
+        end
+
+        % Compute final errors for this dt
+        err1_array(k_idx) = sqrt( sum((rho - 1).^2) / N );
+        err2_array(k_idx) = sqrt( sum_sq_err / (N * M) );
+    end
+
+    %% Plot the errors for this method
+    c = colors{method};
+    
+    % Epsilon 1: Solid line with circle markers
+    plot(dt_array, err1_array, [c '-o'], 'DisplayName', ['Method ' num2str(method) ' (\epsilon_1)'], 'LineWidth', 1.5);
+    
+    % Epsilon 2: Dotted line with square markers
+    plot(dt_array, err2_array, [c ':s'], 'DisplayName', ['Method ' num2str(method) ' (\epsilon_2)'], 'LineWidth', 1.5);
+    
+end
+
+% Finalize plot
+legend('show', 'Location', 'east');
+hold off;
